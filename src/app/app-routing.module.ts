423a49c814f5156a87f84bce6becfb49342f102f
Ezequiel <dev.ezequiel@gmail.com>
2020-08-07 18:42:07 -0300

    agregado de componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+//Components
+import { SiteComponent } from './components/site/site.component'
+import { HomeComponent } from './components/home/home.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'site',
+    component: SiteComponent
+  },
+  {
+    path: '',
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SiteComponent } from './components/site/site.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SiteComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
